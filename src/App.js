f0a184466e2a38b0e540cd53cf2854ab9f36055b
shleshankiborkar3 <shleshankib@gmail.com>
2025-06-07 12:27:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Calculator from './calculator';
+
+function App() {
+    <h2>Calculator</h2>
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
